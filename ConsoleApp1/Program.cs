ec2208070c05bdcf007e74a92520a5360d59b836
Beto Schmidlin <carlos78uk@gmail.com>
2020-04-15 15:37:59 +0100

    ajustes bobos

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,6 +5,7 @@ namespace ConsoleApp1 {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Good Afternoon");
+            for int i <0 i++ 
         }
     }
 }
